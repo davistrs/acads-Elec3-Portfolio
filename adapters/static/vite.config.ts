a8b5b9efdfffffaf6a1114a1c96d779f6f3836cb
Mark Siazon <markangelosiazon@gmail.com>
2025-12-04 08:48:15 +0800

    feat: add GitHub Actions workflow for static site deployment and configure Vite static adapter

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://yoursite.qwik.dev",
+        origin: "https://mark-siazon.github.io/acads-Elec3-Portfolio",
       }),
     ],
   };
